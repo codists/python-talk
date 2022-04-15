e7bc9596ce1ce9c2c354d5c8bf9961b068f24ce0
qiujiajin <609799548@qq.com>
2022-04-15 21:02:47 +0800

    update entrypoint add start before run flask db upgrade

---
+++ backend/python_talk/entrypoint.py
@@ -0,0 +1,27 @@
+import time
+import subprocess
+
+from app import app, db
+
+
+def wait_db(application, database, interval=3):
+    while True:
+        try:
+            with application.app_context():
+                with database.engine.connect():
+                    application.logger.info('[start flask] database connected.')
+        except Exception as e:
+            application.logger.warning('[start flask] wait db ...')
+            application.logger.exception(e)
+            time.sleep(interval)
+        else:
+            break
+
+
+def run():
+    wait_db(app, db)
+    subprocess.run('flask db upgrade && flask run', shell=True)
+
+
+if __name__ == '__main__':
+    run()
