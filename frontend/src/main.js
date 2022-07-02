14dfaecbd044a64e489d21fcf5b051d1bbb25192
qiujiajin <609799548@qq.com>
2022-07-02 21:34:41 +0800

    layout init

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css' 
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+
+createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
---
+++ frontend/src/views/IndexView.vue
@@ -0,0 +1,102 @@
+<template>
+  <el-container>
+    <el-header class="header">
+        <div class="nav clearfix">
+            <div class="left clearfix">
+                <div class="logo">
+                    <img src="@/assets/Python_logo_and_wordmark.svg" alt="lgoo" height="42">
+                </div>
+                <div class="index"><a href="#">首页</a></div>
+            </div>
+
+            <div class="right">
+                <ul class="clearfix">
+                    <li><a href="#">登录</a></li>
+                    <li><a href="#">注册</a></li>
+                </ul>
+            </div>
+        </div>
+    </el-header>
+    <el-main class="main">
+    </el-main>
+    <el-footer class="footer">
+        <span>Copyright ©2001-2022.  Github</span>
+    </el-footer>
+  </el-container>
+</template>
+
+<style>
+.header {
+    width: 80%;
+    height: 42px;
+    padding: 0;
+    margin: 48px auto 12px;
+}
+
+.nav {
+    background-color: #273643;
+    color: white;
+    line-height: 42px;
+    height: 42px;
+}
+
+.clearfix::after {
+    content: '';
+    display: table;
+    clear: both;
+}
+
+.flex-grow {
+  flex-grow: 1;
+}
+
+.nav .left, .nav .left div {
+    float: left;
+}
+
+.nav .left .index a {
+    display: block;
+    color: white;
+    text-decoration: none;
+    width: 100%;
+    padding-left: 55px;
+    padding-right: 55px;
+}
+
+.nav .right {
+    float: right;
+}
+
+.nav .right ul li {
+    float: left;
+    list-style: none;
+}
+
+.nav .right ul li  a {
+    text-align: center;
+    color: white;
+    text-decoration: none;
+    display: block;
+    width: 133px;
+}
+
+.main {
+    width: 80%;
+    height: 860px;
+    background-color: azure;
+    margin: 12px auto;
+}
+
+.footer {
+    width: 80%;
+    height: 80px;
+    background-color: #2B5B84;
+    color: white;
+    line-height: 80px;
+    text-align: center;
+    margin: 12px auto;
+}
+</style>
+
+<script setup>
+</script>
