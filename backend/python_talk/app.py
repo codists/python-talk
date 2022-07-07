86673d9fdefd181ee28b4ae0a4995eaed398b1fb
codists <codists@foxmail.com>
2022-07-07 20:33:35 +0800

    change app1 to app

--- backend/python_talk/app.py
+++ backend/python_talk/app.py
@@ -1,28 +1,27 @@
 from flask import Flask, Blueprint
 
 import config
-import  models  # noqa
+import models  # noqa
 from exts import db, cors, migrate, mail
 from utils.security import identify
 from utils.restful import restful
 from blueprints import user
 
-
 app = Flask(__name__)
 app.config.from_object(config)
 
 # 第三方扩展初始化
-db.init_app(app1)
-cors.init_app(app1)
-migrate.init_app(app1, db)
-mail.init_app(app1)
+db.init_app(app)
+cors.init_app(app)
+migrate.init_app(app, db)
+mail.init_app(app)
 
 # 注册蓝图
 api = Blueprint('api', __name__, url_prefix='/api')
 api.register_blueprint(user.user_bp)
 
 # 获取token
-app1.before_request(identify)
+app.before_request(identify)
 
 
 @api.errorhandler(400)
@@ -30,9 +29,9 @@ def api_bad_request(error):
     return restful.error('Bad request'), 400
 
 
-app1.register_blueprint(api)
+app.register_blueprint(api)
 
 
-@app1.get('/')
+@app.get('/')
 def index():
     return 'Python Talk!'
--- backend/python_talk/tasks.py
+++ backend/python_talk/tasks.py
@@ -1,11 +1,16 @@
 """
 flask 是生产者，task是消费者
 """
-from exts import mail
 from flask_mail import Message
+
+from exts import mail
+from celery_app import make_celery
 from app import app
 
-@celery_app.task
+cel = make_celery(app)
+
+
+@cel.task
 def async_send_email(msg):
     msg = Message(msg)
     mail.send(msg)
