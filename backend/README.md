c54bfecb6e7ea09657fd0b2c986d0dea088a9ba6
codists <codists@foxmail.com>
2022-07-08 08:34:46 +0800

    recreate backend

--- backend/README.md
+++ backend/README.md
@@ -1,3 +1,18 @@
-### 运行后端开发环境
-1. 切换工作目录，处于 backend/ 下
-2. docker-compose up 启动
+# How to run project 
+- clone
+```python
+$ git clone https://github.com/codists/python-talk.git
+```
+- create and activate virtual environment
+```python
+$ cd python-talk/backend/
+python-talk/backend$python-talk$ python -m venv venv
+```
+- install requirements.txt
+```python
+python-talk/backend$ pip install -r requirements.txt
+```
+- run 
+```python
+python-talk/backend$ flask run
+```
--- backend/python_talk/config.py
+++ backend/python_talk/config.py
@@ -1,37 +1,40 @@
 """
-flask配置参考：
+1.flask configuration reference：https://flask.palletsprojects.com/en/latest/config/
+2.from_object() reference：https://flask.palletsprojects.com/en/latest/config/#development-production
 """
 import os
+import sys
 
-# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@db:3306/python_talk?charset=utf8'
-SQLALCHEMY_DATABASE_URI = "sqlite:///test.sqlite"
+# root directory
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-# 问题：相对路径如何表示
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-# 用于生成JWT
-SECRET_KEY = '09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7'
-ALGORITHM = 'HS256'
-
-# 邮件配置
-# 谷歌邮箱端口：587， 网易邮箱端口：25， QQ邮箱端口：
-MAIL_SERVER = 'smtp.163.com'
-MAIL_PORT = 25
-MAIL_USE_TLS = True
-MAIL_USE_SSL = False
-MAIL_USERNAME = 'codists@163.com'
-MAIL_PASSWORD = 'KFUQPGLXRYVMWZMG'
-# MAIL_DEFAULT_SENDER = 'noreply@pythontalk.com'
-MAIL_DEFAULT_SENDER = 'codists@163.com'
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
 
 
-"""
-celery配置文件参考：https://docs.celeryq.dev/en/stable/userguide/configuration.html
-"""
+class Config(object):
+    TESTING = False
 
-# Broker settings.
-# broker_url = 'amqp://guest:guest@localhost:5672//'
-broker_url = 'redis://localhost:6379/1'
 
-# Using the database to store task state and results.
-result_backend = 'redis://localhost:6379/2'
+class ProductionConfig(Config):
+    DATABASE_URI = 'mysql://user@localhost/foo'
+
+
+class DevelopmentConfig(Config):
+    # turn on debug
+    DATABASE_URI = prefix + os.path.join(basedir, 'dev.db')
+
+
+class TestingConfig(Config):
+    DATABASE_URI = 'sqlite:///:memory:'
+    TESTING = True
+
+
+config_class_name = {
+    'production': ProductionConfig,
+    'development': DevelopmentConfig,
+    'testing': TestingConfig
+}
