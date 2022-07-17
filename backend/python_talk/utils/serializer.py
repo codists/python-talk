373ab6cd4decb795c28cf2014dc4817d54caeb5b
qiujiajin <609799548@qq.com>
2022-07-17 16:33:49 +0800

    refactor serializer

--- backend/python_talk/utils/serializer.py
+++ backend/python_talk/utils/serializer.py
@@ -1,7 +1,7 @@
-from datetime import datetime
-from collections.abc import Iterable
-
 """
+序列化工具
+
+参考
 Python          JSON
 dict            object
 tuple, list     array
@@ -9,7 +9,30 @@ str             string
 int, float      number
 bool            boolean
 None            null
+
+使用
+from python_talk.utils.serializer import serializer
+
+serializer(user, ['id', 'username', 'email'])
+
+# 如果 fields 为空，会去读取模型类定义的 __serializer__
+serializer(user)
+
+# 指定某种类型统一序列化
+serializer(user, funcs={
+    datetime: lambda dt: dt.strftime('%Y-%m-%d %H:%M:%S')
+})
+
+# 自定义序列化后的格式
+serializer(user, ['id'], funcs={
+    'id': lambda id_: f'userId:{id_}'
+})
 """
+
+from datetime import datetime
+from collections.abc import Iterable
+
+__all__ = ['serializer', ]
 
 BASE_TYPES = (
     dict,
@@ -21,11 +44,10 @@ BASE_TYPES = (
 )
 
 SERIALIZER = '__serializer__'
-
 DATETIME_FORMATTER = '%Y-%m-%d %H:%M:%S'
 
 
-def __check_type(instance, types=BASE_TYPES):
+def check_type(instance, types=BASE_TYPES):
     """
     判断实例是否属于某些类型（不包含它们的子类）
     注意与 isinstance 的区别
@@ -37,37 +59,43 @@ def __check_type(instance, types=BASE_TYPES):
     return False
 
 
-def serializer(instance_instances, fields=None, funcs=None, top=None):
-    """
-    Usage:
-        user = User.query.get(1)
-        serializer(user, ['username', {'articles': ['id', 'title']}])
-    """
-    if isinstance(instance_instances, Iterable):
-        return [serializer(instance, fields, funcs, top=top) for instance in instance_instances]
+class Serializer:
 
-    instance = instance_instances
-    fields = fields or getattr(instance, SERIALIZER, [])
+    def __init__(self, default_funcs=None):
+        self.default_funcs = default_funcs or {
+            datetime: lambda dt: dt.strftime(DATETIME_FORMATTER),
+        }
 
-    funcs = funcs or {}
-    funcs.update({
-        datetime: lambda dt: dt.strftime(DATETIME_FORMATTER),
-    })
+    def __call__(self, instance_instances, fields=None, funcs=None):
+        return self.serializer(instance_instances, fields, funcs)
 
-    result = {}
+    def serializer(self, instance_instances, fields=None, funcs=None, top=None):
+        if isinstance(instance_instances, Iterable):
+            return [self.serializer(instance, fields, funcs, top=top) for instance in instance_instances]
 
-    for field_name in fields:
-        if isinstance(field_name, str):
-            field_value = getattr(instance, field_name)
-            if not __check_type(field_value):
+        instance = instance_instances
+        fields = fields or getattr(instance, SERIALIZER, [])
+        funcs = funcs or {}
+        result = {}
+
+        for field_name in fields:
+            if isinstance(field_name, str):
+                value = getattr(instance, field_name)
                 func_key = f'{top}.{field_name}' if top else field_name
-                func = funcs.get(func_key) or funcs.get(type(field_value))
-                field_value = func(field_value) if func else serializer(field_value, funcs=funcs, top=field_name)
+                func = funcs.get(func_key) or funcs.get(type(value)) or self.default_funcs.get(type(value))
+                if func:
+                    value = func(value)
 
-            result[field_name] = field_value
+                if not check_type(value):
+                    value = self.serializer(value, funcs=funcs, top=field_name)
 
-        elif isinstance(field_name, dict):
-            for f, child_fields in field_name.items():
-                result[f] = serializer(getattr(instance, f), child_fields, funcs, top=f)
+                result[field_name] = value
 
-    return result
+            elif isinstance(field_name, dict):
+                for f, child_fields in field_name.items():
+                    result[f] = self.serializer(getattr(instance, f), child_fields, funcs, top=f)
+
+        return result
+
+
+serializer = Serializer()
