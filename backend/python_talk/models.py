856722b4dc8e6af6a34572422f42888689af2189
codists <codists@foxmail.com>
2025-07-06 23:02:17 +0800

    change __password to password

--- backend/python_talk/models.py
+++ backend/python_talk/models.py
@@ -32,7 +32,8 @@ class User(Base):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False, unique=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
-    __password = db.Column('password', db.String(255), nullable=False)
+    # __password = db.Column('password', db.String(255), nullable=False)
+    password = db.Column('password', db.String(255), nullable=False)
     avatar = db.Column(db.String(512))
 
     __serializer__ = ['id', 'username', 'email', 'avatar', 'created_at']
@@ -43,13 +44,13 @@ class User(Base):
         self.password = password
         self.avatar = avatar
 
-    @property
-    def password(self):
-        raise AttributeError('password field is not allowed to access')
+    # @property
+    # def password(self):
+    #     raise AttributeError('password field is not allowed to access')
 
-    @password.setter
-    def password(self, value):
-        self.__password = generate_password_hash(value)
+    # @password.setter
+    # def password(self, value):
+    #     self.__password = generate_password_hash(value)
 
     def check_password(self, value):
         return check_password_hash(self.__password, value)
