89e82d16d4fcbf52ed726ff5d043ba6a4cd6cff8
unknown <codists@foxmail.com>
2025-12-28 16:56:03 +0800

    solve alembic can not find model in scrapy project

--- backend/python_talk/models/__init__.py
+++ backend/python_talk/models/__init__.py
@@ -1,3 +1,7 @@
 # @Filename: __init__.py
 # @Author: codists
 # @Created: 2025-07-14 23:04:11
+
+# 因为 Book 模型实在另外一个项目 booksbot 用，alembic 检测不到，所以导入到这里，让 alembic 检测到
+from .user import User
+from .book import Book
